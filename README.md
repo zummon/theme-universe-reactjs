7c44bf5928ec3f5cf7db9555bdf67ded35a464cb
Teerapat Anantarattanachai <zummon.space@gmail.com>
2023-04-09 15:32:36 +0000

    Update 4 files

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
+### Dev tools
+
+[Bootstrap](https://getbootstrap.com/)
+
+[Icons](https://icons.getbootstrap.com/)
+
+[unDraw](https://undraw.co/)
+
+---
 [Made by zummon](https://zummon.page/)
-
-## Dev tools
-
-- [Bootstrap](https://getbootstrap.com/)
-- [Icons](https://icons.getbootstrap.com/)
-- [unDraw](https://undraw.co/)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import React from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const rootElement = document.getElementById("_root");
-ReactDOM.render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-	rootElement
-);
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
